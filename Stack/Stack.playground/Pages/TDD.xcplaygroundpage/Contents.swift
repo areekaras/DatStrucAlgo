ae2cc0bf4e6e2e828904471f4880b90d3ff425aa
areekaras <shibili.areekara@gmail.com>
2022-02-12 10:30:51 +0530

    Added TDD playground page and write down the requirements

---
+++ Stack/Stack.playground/Pages/TDD.xcplaygroundpage/Contents.swift
@@ -0,0 +1,28 @@
+import Foundation
+import XCTest
+
+
+
+
+
+
+class StackTests: XCTestCase {
+    
+}
+
+StackTests.defaultTestSuite.run()
+
+
+//Use cases
+
+// Stack characteristics
+    // 1. store number of elements
+    // 2. Can be empty
+    // 3. push(_:) must enter elements into the last position of stack
+    // 4. pop() should remove last element of the stack and return it
+    // 5. pop() should return nil if stack is empty
+    // 6. peek() will return last element of stack
+    // 7. peek() should return nil if no elements in the stack
+    // 8. isEmpty will return whether stack is empty or not
+    // 9. count will return number of elements in the stack
+
